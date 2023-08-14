fdfdec630218e86efd60b584adc0dedcab0a1c17
AustinZhao0308 <1159107202@qq.com>
2023-08-14 19:35:25 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# 首先，将PyOfficeRobot模块导入到我们的代码块中。
 from PyOfficeRobot.core.WeChatType import *
 
-# 获取当前微信客户端
+keywords = {
+    "我要报名": "你好，这是报名链接：www.python-office.com"
+}
+
 wx = WeChat()
-# 获取会话列表
 wx.GetSessionList()
-# 向某人发送消息（以`文件传输助手`为例）
-msg = '你好~我是程序员晚枫，感谢点赞 + 转发'
-who = '文件传输助手'
-wx.ChatWith(who)  # 打开`文件传输助手`聊天窗口
+msg = 'test'
+who = 'akinaustin'
+wx.ChatWith(who=who)
 # for i in range(10):
-wx.SendMsg(msg)  # 向`文件传输助手`发送消息：你好~
+wx.SendMsg(msg, who)
+PyOfficeRobot.chat.chat_by_keywords(who, keywords=keywords)
