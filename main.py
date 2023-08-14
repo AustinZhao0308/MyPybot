b7ebffd7306e1188d467ae32c26fc9a541be63ee
AustinZhao0308 <1159107202@qq.com>
2023-08-14 19:45:03 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PyOfficeRobot.core.WeChatType import *
+import PyOfficeRobot
+import time
 
 keywords = {
+    "你好": "你好！我是Akinlan Studio Bot。",
     "我要报名": "你好，这是报名链接：www.python-office.com"
 }
 
@@ -11,4 +14,7 @@ who = 'akinaustin'
 wx.ChatWith(who=who)
 # for i in range(10):
 wx.SendMsg(msg, who)
-PyOfficeRobot.chat.chat_by_keywords(who, keywords=keywords)
+while True:
+    PyOfficeRobot.chat.chat_by_keywords(who, keywords=keywords)
+    time.sleep(1)
+
