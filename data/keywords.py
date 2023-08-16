3ff3054d35fa0f48b1ec9495c7a6024ec848dbcf
AustinZhao0308 <1159107202@qq.com>
2023-08-16 10:50:47 +0800

    update

--- data/keywords.py
+++ data/keywords.py
@@ -1,5 +1,7 @@
 import scripts.Jellyfin as jf
 
 keywords = {
-    "你好": "你好！我是Akinlan Studio Bot。"
+    "你好": "你好！我是Akinlan Studio Bot。",
+    "酒单": "https://www.bilibili.com/video/BV15s4y1X7XB/?spm_id_from=333.337.search-card.all.click&vd_source=83964ecd3d23a7f8b663cd1fa99c2f4f",
+    "理财": "https://quilled-pumpkin-479.notion.site/68a0b7ade2d74a80be51aa4ede1913b8?pvs=4"
 }
